ebffb640a42ac2cdabedf9b12d2c30083f768640
vichitpovgit <v.vichitpov@gmail.com>
2018-06-08 15:33:58 +0700

    First Commit

---
+++ pvdialoglibrary/src/main/java/com/vichit/pvdialoglibrary/OnShowDialog.java
@@ -0,0 +1,12 @@
+package com.vichit.pvdialoglibrary;
+
+import android.content.DialogInterface;
+
+public interface OnShowDialog {
+
+    void setOnNegativeButton(DialogInterface dialogInterface);
+
+    void setOnPositiveButton(DialogInterface dialogInterface);
+
+
+}
---
+++ pvdialoglibrary/src/main/java/com/vichit/pvdialoglibrary/PVDialog.java
@@ -0,0 +1,50 @@
+package com.vichit.pvdialoglibrary;
+
+import android.content.Context;
+import android.support.v7.app.AlertDialog;
+
+public class PVDialog {
+
+    private AlertDialog.Builder builder;
+    private Context context;
+    private OnShowDialog onShowDialog;
+    private boolean cancelable;
+
+    PVDialog(Context context, String title, String message) {
+        this.context = context;
+        if (builder == null) {
+            builder = new AlertDialog.Builder(context);
+            builder.setTitle(title);
+            builder.setMessage(message);
+        }
+    }
+
+    public void setActionButton(String positiveText, String negativeText, OnShowDialog onShowDialog) {
+
+        this.onShowDialog = onShowDialog;
+
+        if (!isCancelable())
+            builder.setCancelable(false);
+        else if (isCancelable())
+            builder.setCancelable(true);
+
+        builder.setPositiveButton(positiveText, (dialogInterface, i) -> {
+            onShowDialog.setOnPositiveButton(dialogInterface);
+        });
+
+        builder.setNegativeButton(negativeText, (dialogInterface, i) -> {
+            onShowDialog.setOnNegativeButton(dialogInterface);
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private boolean isCancelable() {
+        return cancelable;
+    }
+
+    public void setCancelable(boolean cancelable) {
+        this.cancelable = cancelable;
+    }
+}
